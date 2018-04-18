9d3fc85b2dc5493f3b41fed24f76e3e11f9fd24d
inkcity <920907676@qq.com>
2018-04-18 18:07:18 +0800

    form放到根vue中，并且不是在template中定义，直接放到html中

--- login/form.js
+++ login/form.js
@@ -1,56 +1,22 @@
-Vue.component('login',{
-    template: `
-        <div class="container">
-            <form class="form-horizontal" method="post">
-                <div class="form-group">
-                    <label for="inputEmail3" class="col-lg-2 control-label">Email</label>
-                    <div class="col-lg-4">
-                        <input v-model='username' type="email" class="form-control" id="inputEmail3" name="username" placeholder="Email">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-lg-2 control-label">Password</label>
-                    <div class="col-lg-4">
-                        <input v-model='password' type="password" class="form-control" id="inputPassword3" name="password" placeholder="Password">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-lg-offset-2 col-lg-2">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox"> Remember me
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-lg-offset-2 col-lg-2">
-                        <button v-on:click='submit' type="button" class="btn btn-default">Sign in</button>
-                    </div>
-                </div>
-            </form>
-        </div>`,
-    data: function() {
-        return {
+new Vue({
+    el: '#index',
+    data: {
             username: '',
-            password: ""
-        };
+            password: '',
+            seen: true
     },
     methods: {
         submit: function() {
             var params = new URLSearchParams();
             params.append('username', this.username);
             params.append('password', this.password)
+            var vm = this
             axios.post('http://localhost:8080/demo/login', params
             ).then(function (response) {
-                console.log(response);
+                vm.seen = !response.data.msg;
             }).catch(function (error) {
                 console.log(error);
-            })
+            });
         }
     }
-})
-
-new Vue({
-    el: '#index'
 })
